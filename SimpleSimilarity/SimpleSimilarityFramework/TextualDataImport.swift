02eba500831faa40035d27b19514aa80e445dc2c
Julius Bahr <Julius.Bahr@partner.bmw.de>
2017-08-20 18:06:55 +0200

    Initial Xcode project commit

---
+++ SimpleSimilarity/SimpleSimilarityFramework/TextualDataImport.swift
@@ -0,0 +1,9 @@
+//
+//  TextualDataImport.swift
+//  SimpleSimilarityFramework
+//
+//  Created by Julius Bahr on 20.08.17.
+//  Copyright © 2017 Julius Bahr. All rights reserved.
+//
+
+import Foundation
