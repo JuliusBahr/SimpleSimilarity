e5776db6b5a70c763d0c8caf21c56f171a2cdd68
Julius Bahr <Julius.Bahr@partner.bmw.de>
2017-08-20 18:54:43 +0200

    - Added basic data structures for file loading - Started work on unit test for csv reading

--- SimpleSimilarity/SimpleSimilarityFramework/TextualDataImport.swift
+++ SimpleSimilarity/SimpleSimilarityFramework/TextualDataImport.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+
+/// Error object thrown when reading a file fails
+struct FileReadError: Error {}
+
+/// Data structure for a single textual input
+struct TextualData {
+    /// The input string
+    let inputString: String
+
+    /// The origin where this string is found. I.e. a book or a webpage or software
+    let origin: String?
+}
+
+
+/// Base protocol used by objects that load data for the textual similarity search
+protocol TextualDataImport {
+
+    /// Load's the content of the given file
+    ///
+    /// - Parameter fileName: this file is being read
+    /// - Throws: a FileReadError error when loading the file fails
+    func loadFile(at fileName: String) throws
+
+    /// The contents of the file that has been loaded
+    /// - Precondition: a file must have been loaded before
+    var fileContents: [TextualData]? {get}
+}
+
+struct CSVImport: TextualDataImport {
+    fileprivate var csvFileContents: [TextualData]?
+
+    var fileContents: [TextualData]? {
+        get {
+            return csvFileContents
+        }
+    }
+
+    func loadFile(at fileName: String) throws {
+        if FileManager.default.fileExists(atPath: fileName) {
+            let fileContents = FileManager.default.contents(atPath: fileName)
+            
+        } else {
+            throw FileReadError()
+        }
+    }
+
+}
--- SimpleSimilarity/SimpleSimilarityFrameworkTests/SimpleSimilarityFrameworkTests.swift
+++ SimpleSimilarity/SimpleSimilarityFrameworkTests/SimpleSimilarityFrameworkTests.swift
@@ -21,16 +21,9 @@ class SimpleSimilarityFrameworkTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testCsvImport() {
+        let csvImporter = CSVImport()
+        Bundle.main.path(forResource: "sample", ofType: "csv")
     }
     
 }
