64881246261ae245ac8f56dc6154f7349dcb5fa9
Julius Bahr <Julius.Bahr@partner.bmw.de>
2018-03-25 12:17:59 +0200

    Data structure to track strings that map to the same bag of words
    - added data structure

---
+++ SimpleSimilarity/SimpleSimilarityFramework/StringsForBagOfWords.swift
@@ -0,0 +1,22 @@
+//
+//  StringsForBagOfWords.swift
+//  SimpleSimilarityFramework
+//
+//  Created by Julius Bahr on 19.02.18.
+//  Copyright © 2018 Julius Bahr. All rights reserved.
+//
+
+import Foundation
+
+struct StringForBagsOfWords {
+    static private let stringsForBagOfWords: [Set<String>:[String]] = [:]
+    
+    static func strings(for bagOfWords: Set<String>) -> [String] {
+        return [""]
+    }
+    
+    static func add(corpusEntry: CorpusEntry) {
+        
+        
+    }
+}
