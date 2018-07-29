cfaa0b9be4a68c4c5e8d18f82b04634c11ef4d00
Julius Bahr <Julius.Bahr@partner.bmw.de>
2018-07-29 11:24:55 +0200

    Support for ArrayOfIndices
    - Create base class ArrayOfIndices

---
+++ SimpleSimilarityFramework/ArrayOfIndices.swift
@@ -0,0 +1,16 @@
+//
+//  ArrayOfIndices.swift
+//  SimpleSimilarityFramework
+//
+//  Created by Julius Bahr on 29.07.18.
+//  Copyright © 2018 Julius Bahr. All rights reserved.
+//
+
+import Foundation
+
+class ArrayOfIndices {
+    init(uniueValues: Set<AnyHashable>) {
+        
+    }
+
+}
