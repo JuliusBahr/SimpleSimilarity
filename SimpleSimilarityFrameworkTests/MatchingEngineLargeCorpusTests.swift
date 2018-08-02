f2d2aa4708e84c2efbb48e6dfd2a96496b9ba8f0
Julius Bahr <Julius.Bahr@partner.bmw.de>
2018-08-02 17:13:35 +0200

    Support for ArrayOfIndices
    - Reduce amount of code repetition in MatchingEngineLargeCorpusTests
    - Add unit test to test test performance of search

--- SimpleSimilarityFrameworkTests/MatchingEngineLargeCorpusTests.swift
+++ SimpleSimilarityFrameworkTests/MatchingEngineLargeCorpusTests.swift
@@ -10,32 +10,49 @@ import XCTest
 @testable import SimpleSimilarityFramework
 
 class MatchingEngineLargeCorpusTests: XCTestCase {
-    
-    func testQueryFoundInCorpusNotExhaustive() {
-    
+
+    static var matchingEngine: MatchingEngine?
+
+    override class func setUp() {
+        super.setUp()
+
+        setupMatchingEngineWithLargeInput() {} // this is less then ideal as the matching engine will be filled at least twice in parallel as we cannot block the first test to execute till the matching engine is filled
+    }
+
+    class func setupMatchingEngineWithLargeInput(completion: @escaping () -> Void) {
+        if let localMatchingEngine = self.matchingEngine, localMatchingEngine.isFilled {
+            completion()
+            return
+        }
+
         guard let csvPath = Bundle.main.path(forResource: "newspaper", ofType: "csv") else {
             return
         }
-        
+
         var csvImporter = NewspaperCorpusImport()
         do {
             try csvImporter.loadFile(at: csvPath)
         } catch {
             print("Reading the csv file caused an exception.")
         }
-        
-        let matchingEngine = MatchingEngine()
-        
+
+        self.matchingEngine = MatchingEngine()
+
         guard let fileContents = csvImporter.fileContents else {
             return
         }
-        
+
+        self.matchingEngine!.fillMatchingEngine(with: fileContents, completion: completion)
+    }
+    
+    func testQueryFoundInCorpusNotExhaustive() {
+
         let asyncExpectation = expectation(description: "asyncWait")
         
-        matchingEngine.fillMatchingEngine(with: fileContents) {
+        MatchingEngineLargeCorpusTests.setupMatchingEngineWithLargeInput() {
             let yellowTailedTuna = TextualData(inputString: "Der Bundeskanzler reiste nach Algerien", origin: nil, originObject: nil)
             
-            try? matchingEngine.bestResult(for: yellowTailedTuna, exhaustive: false, resultFound: { (result) in
+            try? MatchingEngineLargeCorpusTests.matchingEngine?.bestResult(for: yellowTailedTuna, exhaustive: false, resultFound: { (result) in
                 guard let result = result else {
                     XCTFail("No result found")
                     return
@@ -53,30 +70,13 @@ class MatchingEngineLargeCorpusTests: XCTestCase {
     }
     
     func testQueryFoundInCorpusExhaustive() {
-        
-        guard let csvPath = Bundle.main.path(forResource: "newspaper", ofType: "csv") else {
-            return
-        }
-        
-        var csvImporter = NewspaperCorpusImport()
-        do {
-            try csvImporter.loadFile(at: csvPath)
-        } catch {
-            print("Reading the csv file caused an exception.")
-        }
-        
-        let matchingEngine = MatchingEngine()
-        
-        guard let fileContents = csvImporter.fileContents else {
-            return
-        }
-        
+
         let asyncExpectation = expectation(description: "asyncWait")
         
-        matchingEngine.fillMatchingEngine(with: fileContents) {
+        MatchingEngineLargeCorpusTests.setupMatchingEngineWithLargeInput() {
             let expectedBestMatch = TextualData(inputString: "Das ist die wichtigste Lektion für Amerika aus dem Jahr 2002", origin: nil, originObject: nil)
             
-            try? matchingEngine.bestResult(for: expectedBestMatch, exhaustive: true, resultFound: { (result) in
+            try? MatchingEngineLargeCorpusTests.matchingEngine?.bestResult(for: expectedBestMatch, exhaustive: true, resultFound: { (result) in
                 guard let result = result else {
                     XCTFail("No result found")
                     return
@@ -96,34 +96,56 @@ class MatchingEngineLargeCorpusTests: XCTestCase {
     
     func testQueryNotFoundInCorpus() {
         
+        let asyncExpectation = expectation(description: "asyncWait")
+        
+        MatchingEngineLargeCorpusTests.setupMatchingEngineWithLargeInput() {
+            let noMatchQuery = TextualData(inputString: "great sashimi dish", origin: nil, originObject: nil)
+            
+            try? MatchingEngineLargeCorpusTests.matchingEngine?.bestResult(for: noMatchQuery, exhaustive: true, resultFound: { (result) in
+                XCTAssertNil(result, "Result found where no result was expected")
+                asyncExpectation.fulfill()
+            })
+        }
+        
+        waitForExpectations(timeout: 60)
+    }
+
+    func testZManySearches() {
+        let asyncExpectation = expectation(description: "asyncWait")
+
+        // get queries
         guard let csvPath = Bundle.main.path(forResource: "newspaper", ofType: "csv") else {
             return
         }
-        
+
         var csvImporter = NewspaperCorpusImport()
         do {
             try csvImporter.loadFile(at: csvPath)
         } catch {
             print("Reading the csv file caused an exception.")
         }
-        
-        let matchingEngine = MatchingEngine()
-        
+
         guard let fileContents = csvImporter.fileContents else {
             return
         }
-        
-        let asyncExpectation = expectation(description: "asyncWait")
-        
-        matchingEngine.fillMatchingEngine(with: fileContents) {
-            let noMatchQuery = TextualData(inputString: "great sashimi dish", origin: nil, originObject: nil)
-            
-            try? matchingEngine.bestResult(for: noMatchQuery, exhaustive: true, resultFound: { (result) in
-                XCTAssertNil(result, "Result found where no result was expected")
-                asyncExpectation.fulfill()
+
+        let queries = fileContents[0...100]
+
+        MatchingEngineLargeCorpusTests.setupMatchingEngineWithLargeInput() {
+
+            queries.forEach({ (query) in
+                try? MatchingEngineLargeCorpusTests.matchingEngine?.bestResult(for: query, exhaustive: true, resultFound: { (result) in
+                    if query.inputString.count > 3 {
+                        XCTAssertNotNil(result, "We should find in the corpus what we previously added:\n query: \(query.inputString)")
+                        XCTAssert(result?.quality ?? 0.0 > 0.98, "Result quality is too low: \(result?.quality)")
+                    }
+                })
             })
+
+            asyncExpectation.fulfill()
+
         }
-        
+
         waitForExpectations(timeout: 60)
     }
     
